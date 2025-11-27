b6be2ac45ff22d559624cc9c3c85707d7f8367ab
pworkman37 <pjw2@hood.edu>
2025-11-27 15:52:13 -0500

    Update InputConstraints.java

--- InputConstraints.java
+++ InputConstraints.java
@@ -1,1 +1,17 @@
+public class Constraint {
 
+    String raw;
+
+    public Constraint(String raw) {
+        this.raw = raw;
+    }
+
+    public boolean contains(String keyword) {
+        return raw.toLowerCase().contains(keyword.toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return raw;
+    }
+}
