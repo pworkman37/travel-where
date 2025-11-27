a7033ee13e36e27a3bca1f217696fc2fc9c09eae
pworkman37 <pjw2@hood.edu>
2025-11-27 15:50:57 -0500

    Update OutputOptions.java

--- OutputOptions.java
+++ OutputOptions.java
@@ -1,1 +1,26 @@
+public class TravelOption {
+    private String description;
+    private double cost;
+    private String lodging;
+    private String dates;
+    private int travelers;
+
+    public TravelOption(String description, double cost, String lodging, String dates, int travelers) {
+        this.description = description;
+        this.cost = cost;
+        this.lodging = lodging;
+        this.dates = dates;
+        this.travelers = travelers;
+    }
+
+    @Override
+    public String toString() {
+        return "Description: " + description +
+                "\nEstimated Cost: $" + cost +
+                "\nLodging: " + lodging +
+                "\nDates: " + dates +
+                "\nTravelers: " + travelers +
+                "\n------------------------------\n";
+    }
+}
 
